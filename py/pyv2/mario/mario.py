b588566e4eeab425fcb5a52e70751376fe169d59
808ms <106570833+808ms@users.noreply.github.com>
2023-01-19 13:14:09 -1000

    Delete py/pyv2 directory

--- py/pyv2/mario/mario.py
+++
@@ -1,9 +0,0 @@
-from cs50 import get_int
-
-height = 0
-
-while height not in range(1, 9):
-    height = get_int("Height: ")
-
-for row in range(1, height + 1):
-    print(" " * (height - row) + "#" * row + "  " + "#" * row)
--- py/pyv2/twttr/twttr.py
+++
@@ -1,8 +0,0 @@
-ans = input("Input: ")
-print("Output: ", end="")
-
-for letter in ans:
-
-    if not letter.lower() in ['a','e','i','o','u']:
-        print(letter, end="")
-print()
